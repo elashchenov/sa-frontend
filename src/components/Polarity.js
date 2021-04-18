0a1dd78a47ec744f7a7fc4a3c942e0e3197ef9ec
Eugene Lashchenov <44722533+lashchenov123@users.noreply.github.com>
2021-04-18 19:11:53 +0300

    Update Polarity.js

--- src/components/Polarity.js
+++ src/components/Polarity.js
@@ -5,18 +5,18 @@ class Polarity extends Component {
 
     propTypes = {
         sentence: PropTypes.string.isRequired,
-        polarity: PropTypes.string.isRequired
+        polarity: PropTypes.number.isRequired
     };
 
     render() {
         const green = Math.round((this.props.polarity + 1) * 128);
         const red = 255 - green;
         const textColor = {
-            backgroundColor: 'rgb(0, 255, 0)',
+            backgroundColor: 'rgb(' + red + ', ' + green + ', 0)',
             padding: '15px'
         };
 
-        return <div style={textColor}>This is cat! </div>
+        return <div style={textColor}>"{this.props.sentence}" has polarity of {this.props.polarity} </div>
     }
 }
 
